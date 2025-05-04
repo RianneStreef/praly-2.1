0449de69dbc1d1cd0aa9e7b3abffb8f5ddcc8958
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2025-05-04 09:21:43 +0200

    mobile first

--- src/pages/velo.jsx
+++ src/pages/velo.jsx
@@ -22,10 +22,13 @@ const VeloPage = (props) => {
       <div className="header-placeholder" />
       <h1 className="velo">{languageToUse.velo}</h1>
       <div className="logos-velo">
-        <Link to="https://www.francevelotourisme.com/accueil-velo">
+        <Link
+          to="https://www.francevelotourisme.com/accueil-velo"
+          target="_blank"
+        >
           <img src={logoVelo} className="logo-velo" alt="logo Accueil Vélo" />
         </Link>
-        <Link to="https://www.dolce-via.com/fr/">
+        <Link to="https://www.dolce-via.com/fr/" target="_blank">
           <img
             src={logoDolceVita}
             className="logo-velo"
@@ -42,53 +45,70 @@ const VeloPage = (props) => {
         <span>{languageToUse.introVelo2}</span>
         <span>
           {" "}
-          <Link to="https://www.ardeche-a-velo.com">
+          <Link to="https://www.ardeche-a-velo.com" target="_blank">
             {languageToUse.introVelo3}{" "}
           </Link>
         </span>
         <span>{languageToUse.introVelo4}</span>
         <span>
           {" "}
-          <Link to="https://www.ardeche-guide.com/voies-douces-d-ardeche">
+          <Link
+            to="https://www.ardeche-guide.com/voies-douces-d-ardeche"
+            target="_blank"
+          >
             {languageToUse.introVelo5}{" "}
           </Link>
         </span>
         <span>{languageToUse.introVelo6}</span>
         <span>
           {" "}
-          <Link to="https://via-ardeche.fr/">{languageToUse.introVelo7} </Link>
+          <Link to="https://via-ardeche.fr/" target="_blank">
+            {languageToUse.introVelo7}{" "}
+          </Link>
         </span>
         <span>
           {" "}
-          <Link to="https://www.dolce-via.com/fr/">
+          <Link
+            to="https://www.ardeche-buissonniere.fr/preparer-et-reserver/bouger-activite-ardeche-centre-privas-lavoulte/voies-douces/voie-verte-privas/"
+            target="_blank"
+          >
             {languageToUse.introVelo8}{" "}
           </Link>
         </span>
         <span> {languageToUse.introVelo9} </span>
         <span>
           {" "}
-          <Link to="https://www.viarhona.com/">
+          <Link to="https://www.viarhona.com/" target="_blank">
             {languageToUse.introVelo10}{" "}
           </Link>
         </span>
         <span> {languageToUse.introVelo11} </span>
         <span>
           {" "}
-          <Link to="https://www.ardeche-guide.com/decouvrir/top-10-des-experiences-incontournables/ardeche-velo/grande-traversee-ardeche/">
+          <Link
+            to="https://www.ardeche-guide.com/decouvrir/top-10-des-experiences-incontournables/ardeche-velo/grande-traversee-ardeche/"
+            target="_blank"
+          >
             {languageToUse.introVelo12}{" "}
           </Link>
         </span>
         <span> {languageToUse.introVelo13} </span>
         <span>
           {" "}
-          <Link to=" https://www.ardeche-guide.com/decouvrir/top-10-des-experiences-incontournables/ardeche-velo/lardeche-a-velo-electrique/">
+          <Link
+            to=" https://www.ardeche-guide.com/decouvrir/top-10-des-experiences-incontournables/ardeche-velo/lardeche-a-velo-electrique/"
+            target="_blank"
+          >
             {languageToUse.introVelo14}{" "}
           </Link>
         </span>
         <span> {languageToUse.introVelo15} </span>
         <span>
           {" "}
-          <Link to="https://www.ardeche-guide.com/preparer/manger-local/pique-niques-goutez-ardeche/">
+          <Link
+            to="https://www.ardeche-guide.com/preparer/manger-local/pique-niques-goutez-ardeche/"
+            target="_blank"
+          >
             {languageToUse.introVelo16}
           </Link>
         </span>
